270e874ad212f8b9083350022bb12a59b5405d7e
rahulsinghal11 <rahullsinghal53@gmail.com>
2021-03-19 16:44:53 +0530

    “v2“

--- test_main.py
+++ test_main.py
@@ -34,8 +34,6 @@ def test_auth(client):
     response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
-
-    assert False
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
